ef0f8f8182a87debbcb855d1b288982f7f6644dc
Yudi Zheng <yudi.zheng@oracle.com>
2011-06-21 15:47:02 +0000

    Static instrumentation using DiSL. Not working until skipping the <init> function.

---
+++ test/src/TestDisl.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
+
+import ch.usi.dag.disl.DiSL;
+
+public class TestDisl {
+	public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException {
+		ClassReader cr = new ClassReader(
+				TestDisl.class.getResourceAsStream("TargetClass.class"));
+		ClassNode cn = new ClassNode();
+		cr.accept(cn, 0);
+		
+		DiSL.class.newInstance().instrument(cn);
+		
+		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+		cn.accept(cw);
+		File f = new File("TargetClass.class");
+		FileOutputStream fos = new FileOutputStream(f);
+		fos.write(cw.toByteArray());
+		fos.flush();
+		fos.close();
+	}
+}
