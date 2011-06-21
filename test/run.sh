5575e44b30a24e1af6cc46276d3fe556b4c74197
Lukáš Marek <marek@d3s.mff.cuni.cz>
2011-06-21 18:26:42 +0000

    improved version of test starting script

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,14 @@
 # /bin/sh
 
-java -javaagent:../lib/jborat-agent.jar -Dch.usi.dag.jborat.liblist="conf/lib.lst" -Dch.usi.dag.jborat.instrumentation="ch.usi.dag.disl.DiSL" -Dch.usi.dag.jborat.codemergerList="conf/codemerger.lst" -Dch.usi.dag.jborat.uninstrumented="uninstrumented" -Dch.usi.dag.jborat.instrumented="instrumented" -Xbootclasspath/p:../lib/Thread_jborat.jar:../lib/jborat-runtime.jar -Ddisl.classes="./bin/AnnotatedClass.class" -cp "./bin/" TargetClass
+EXPECTED_ARGS=1
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "Usage: `basename $0` {test case}"
+    exit
+fi
+
+DISL_CLASS="./bin/ch/usi/dag/disl/test/$1/DiSLClass.class"
+TARGET_CLASS="ch.usi.dag.disl.test.$1.TargetClass"
+
+java -javaagent:../lib/jborat-agent.jar -Dch.usi.dag.jborat.liblist="conf/lib.lst" -Dch.usi.dag.jborat.instrumentation="ch.usi.dag.disl.DiSL" -Dch.usi.dag.jborat.codemergerList="conf/codemerger.lst" -Dch.usi.dag.jborat.uninstrumented="uninstrumented" -Dch.usi.dag.jborat.instrumented="instrumented" -Xbootclasspath/p:../lib/Thread_jborat.jar:../lib/jborat-runtime.jar -Ddisl.classes=${DISL_CLASS} -cp "./bin/" ${TARGET_CLASS}
