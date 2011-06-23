2a73105a8afe9011e20effb6b1b534e71d4104d4
Yudi Zheng <yudi.zheng@oracle.com>
2011-06-23 17:04:32 +0000

    Test case for synthetic local. For now, the flag is defined as "1" before using it in 'precondition'. And the value does pass to 'postcondition'

---
+++ test/src/ch/usi/dag/disl/test/syntheticlocal/DiSLClass.java
@@ -0,0 +1,37 @@
+package ch.usi.dag.disl.test.syntheticlocal;
+
+import ch.usi.dag.disl.annotation.After;
+import ch.usi.dag.disl.annotation.Before;
+import ch.usi.dag.disl.annotation.SyntheticLocal;
+import ch.usi.dag.disl.snippet.marker.BodyMarker;
+
+public class DiSLClass {
+	@SyntheticLocal
+	public static String flag; 
+	
+	@Before(marker = BodyMarker.class, scope = "TargetClass.print(boolean)", order = 0)
+	public static void precondition() {
+		System.out.println("Precondition!");
+		
+		flag = "1";
+		
+		if(flag.equals("1")) {
+			System.out.println("Precondition: This should be printed");
+			return;
+		}
+		
+		System.out.println("Precondition: This should NOT be printed");
+	}
+	
+	@After(marker = BodyMarker.class, scope = "TargetClass.print(boolean)", order = 0)
+	public static void postcondition() {
+		System.out.println("Postcondition!");
+		
+		if(flag.equals("1")) {
+			System.out.println("Postcondition: This should be printed");
+			return;
+		}
+		
+		System.out.println("Postcondition: This should NOT be printed");
+	}
+}
