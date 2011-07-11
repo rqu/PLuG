ff8150884f20cfa785fccde666c48225b89db444
Yudi Zheng <yudi.zheng@oracle.com>
2011-07-11 18:34:11 +0000

    Instrumentation of ProcessorHack

--- src/ch/usi/dag/disl/ProcessorHack.java
+++ src/ch/usi/dag/disl/ProcessorHack.java
@@ -1,15 +1,22 @@
 package ch.usi.dag.disl;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
 
 import ch.usi.dag.disl.snippet.syntheticlocal.SyntheticLocalVar;
+import ch.usi.dag.disl.util.InsnListHelper;
+import ch.usi.dag.disl.weaver.Weaver;
 
 // partial and hacked implementation of processor
 public class ProcessorHack {
@@ -21,7 +28,7 @@ public class ProcessorHack {
 	protected static Map<Type, InsnList> processors = 
 		new HashMap<Type, InsnList>();
 
-	class ArgData {
+	static class ArgData {
 		int pos;
 		int allCount;
 		boolean isStatic;
@@ -52,15 +59,101 @@ public class ProcessorHack {
 		
 		processors.put((Type) ((AnnotationNode) method.invisibleAnnotations.get(0)).values.get(1), method.instructions);
 	}
-	
-	private static List<ArgData> createArgData(MethodNode methodNode) {
 
-		return null;
-	}
-	
-	public static void instrument(MethodNode methodNode, List<SyntheticLocalVar> syntheticLocals) {
+
+	private static List<ArgData> createArgData(MethodNode methodNode) {
+		List<ArgData> list = new LinkedList<ArgData>();
+		Type[] types = Type.getArgumentTypes(methodNode.desc);
+		boolean isStatic = (methodNode.access & Opcodes.ACC_STATIC)>0;
 		
+		ArgData data = null;
+		
+		// The first processor
+		data = new ArgData();
+		data.pos = -1;
+		data.allCount = -1;
+		data.isStatic = isStatic;
+		data.type = Type.VOID_TYPE;
+		data.asmCode = processors.get(data.type);
+		list.add(data);
+		
+		// For each argument
+		for (int i=0; i<types.length; i++){
+			data = new ArgData();
+			data.pos = i;
+			data.allCount = types.length;
+			data.isStatic = isStatic;
+			data.type = types[i];
+			data.asmCode = processors.get(data.type);
+			list.add(data);
+		}
+		
+		return list;
+	}
+
+	public static void instrument(MethodNode methodNode,
+			List<SyntheticLocalVar> syntheticLocals) {
+
 		List<ArgData> instrData = createArgData(methodNode);
+
+		AbstractInsnNode first = methodNode.instructions.getFirst();
+
+		for (ArgData data : instrData) {
+			InsnList ilst = InsnListHelper.cloneList(data.asmCode);
+			AbstractInsnNode temp = null;
+			
+			if (data.allCount == -1){
+				methodNode.instructions.insertBefore(first, ilst);
+				continue;
+			}
+
+			for (AbstractInsnNode instr : ilst.toArray()) {
+				int opcode = instr.getOpcode();
+
+				switch (opcode) {
+				case Opcodes.ILOAD:
+				case Opcodes.LLOAD:
+				case Opcodes.FLOAD:
+				case Opcodes.DLOAD:
+				case Opcodes.ALOAD:
+
+					VarInsnNode varInstr = (VarInsnNode) instr;
+
+					switch (varInstr.var) {
+					// Index of the argument
+					case 0:
+						temp = new LdcInsnNode(data.pos);
+						ilst.insertBefore(instr, temp);
+						ilst.remove(instr);
+						break;
+
+					// Number of the arguments
+					case 1:
+						temp = new LdcInsnNode(data.allCount);
+						ilst.insertBefore(instr, temp);
+						ilst.remove(instr);
+						break;
+
+					// Value
+					case 2:
+						varInstr.var = data.pos + (data.isStatic ? 0 : 1);
+						break;
+
+					default:
+						break;
+					}
+
+					break;
+
+				default:
+					break;
+				}
+			}
+
+			methodNode.instructions.insertBefore(first, ilst);
+		}
+
+		Weaver.static2Local(methodNode, syntheticLocals);
 	}
 
 	/* use case
