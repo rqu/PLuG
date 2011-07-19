33c9230a02a92b3b14fdc733352b8b16e7d04a52
Lukáš Marek <marek@d3s.mff.cuni.cz>
2011-07-19 13:49:30 +0000

    Analysis interface changed - project does not compile

--- src/ch/usi/dag/disl/staticinfo/analysis/Analysis.java
+++ src/ch/usi/dag/disl/staticinfo/analysis/Analysis.java
@@ -2,16 +2,11 @@ package ch.usi.dag.disl.staticinfo.analysis;
 
 public interface Analysis {
 
-	// It isn't mandatory (but it's good practice) to implement this interface
+	// It is mandatory to implement this interface
 	
-	// NOTE: all analysis classes should follow convention:
-	// a) method returns the desired value as the method return value
-	// (not by using modified argument)
+	public void setStaticAnalysisInfo(AnalysisInfo ai);
+	
+	// NOTE: all analysis methods should follow convention:
+	// a) analysis methods does not have parameters
 	// b) return value can be only basic type (+String)
-	// c) method gets only one parameter -> AnalysisInfo
-	// d) methods should be static
-	// e) all classes should share the prefix of the package with this interface
-	// f) javadoc should explain, that the parameter passed by user should be
-	// null (because it will be invoked by DiSL with proper parameter)
-	// h) consistency - method called on same data should return same result
 }
