4077ba51e39c3377367e6f461e1fd72cc3baf140
Lukáš Marek <marek@d3s.mff.cuni.cz>
2011-07-28 13:37:14 +0000

    Introduced class, method and snippet cache StaticContext now supports caching

--- src/ch/usi/dag/disl/staticinfo/analysis/StaticContext.java
+++ src/ch/usi/dag/disl/staticinfo/analysis/StaticContext.java
@@ -1,20 +1,29 @@
 package ch.usi.dag.disl.staticinfo.analysis;
 
+import ch.usi.dag.disl.staticinfo.analysis.cache.ClassCache;
+import ch.usi.dag.disl.staticinfo.analysis.cache.MethodCache;
 import ch.usi.dag.disl.util.Constants;
 
 public class StaticContext extends AbstractStaticAnalysis {
 
+	public StaticContext() {
+		
+		registerCache("getClassName", ClassCache.class);
+		registerCache("getMethodName", MethodCache.class);
+		registerCache("getFullMethodName", MethodCache.class);
+	}
+	
+	public String getClassName() {
+
+		return staticAnalysisData.getClassNode().name;
+	}
+	
 	public String getMethodName() {
 
 		return staticAnalysisData.getMethodNode().name;
 	}
 
-	public String getClassName() {
-
-		return staticAnalysisData.getClassNode().name;
-	}
-
-	public String getFullName() {
+	public String getFullMethodName() {
 
 		return staticAnalysisData.getClassNode().name
 				+ Constants.STATIC_ANALYSIS_METHOD_DELIM
