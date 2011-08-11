b9a0d265ce7ab081e9c1c42cc9bd9debdc18ff41
Yudi Zheng <yudi.zheng@oracle.com>
2011-08-11 15:17:25 +0000

    Test case: 	A disl class that contains exception handler.
    In the previous version, this test case would throw a verify error exception with an inconsistent stack height, and now it should be working.

---
+++ test/src/ch/usi/dag/disl/test/exception/DiSLClass.java
@@ -0,0 +1,17 @@
+package ch.usi.dag.disl.test.exception;
+
+import ch.usi.dag.disl.annotation.Before;
+import ch.usi.dag.disl.snippet.marker.BytecodeMarker;
+
+public class DiSLClass {
+
+	@Before(marker = BytecodeMarker.class, param = "invokevirtual", scope = "TargetClass.bar")
+	public static void precondition() {
+		try {
+			Integer.valueOf("1.0");
+		} catch (Exception e) {
+			System.out.println("exception handler from snippet");
+		}
+	}
+
+}
