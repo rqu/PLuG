9ca89b7d35917a7e513d6f8cd6219cc1aca7589d
Yudi Zheng <yudi.zheng@oracle.com>
2011-08-17 17:54:09 +0000

    Parsing <clinit> to get default value for thread locals.

--- startutil/src/ch/usi/dag/disl/startutil/ClassParser.java
+++ startutil/src/ch/usi/dag/disl/startutil/ClassParser.java
@@ -7,14 +7,25 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.IntInsnNode;
+import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.analysis.Analyzer;
+import org.objectweb.asm.tree.analysis.AnalyzerException;
+import org.objectweb.asm.tree.analysis.Frame;
+import org.objectweb.asm.tree.analysis.SourceValue;
+
+import ch.usi.dag.disl.util.stack.StackUtil;
 
 public class ClassParser {
 
@@ -111,10 +122,137 @@ public class ClassParser {
 		// default value will be set later on
 		return new ThreadLocalVar(field.name, fieldType, inheritable);
 	}
-	
-	private static Map<ThreadLocalVar, Object> setTLVDefaultValues(
-			ClassNode classNode, List<ThreadLocalVar> result) {
+
+	private static void setTLVDefaultValues(ClassNode classNode,
+			List<ThreadLocalVar> result) {
 		// TODO ! parse dafault value from static inti code
-		return null;
+		MethodNode methodNode = null;
+
+		for (MethodNode method : classNode.methods) {
+
+			if (method.name.equals("<clinit>")) {
+				methodNode = method;
+				break;
+			}
+		}
+
+		if (methodNode == null) {
+			return;
+		}
+
+		Analyzer<SourceValue> analyzer = StackUtil.getSourceAnalyzer();
+
+		try {
+			analyzer.analyze(classNode.name, methodNode);
+		} catch (AnalyzerException e) {
+			return;
+		}
+
+		Frame<SourceValue>[] frames = analyzer.getFrames();
+
+		for (ThreadLocalVar var : result) {
+
+			for (int i = 0; i < frames.length; i++) {
+
+				AbstractInsnNode instr = methodNode.instructions.get(i);
+
+				if (instr.getOpcode() != Opcodes.PUTSTATIC) {
+					continue;
+				}
+
+				FieldInsnNode fin = (FieldInsnNode) instr;
+
+				if (!fin.name.equals(var.getName())) {
+					continue;
+				}
+
+				Set<AbstractInsnNode> sources = frames[i].getStack(frames[i]
+						.getStackSize() - 1).insns;
+
+				if (sources.size() != 1) {
+					continue;
+				}
+
+				AbstractInsnNode source = sources.iterator().next();
+
+				switch (source.getOpcode()) {
+				case Opcodes.ACONST_NULL:
+					var.setDefaultValue("null");
+					break;
+
+				case Opcodes.ICONST_M1:
+					var.setDefaultValue("-1");
+					break;
+
+				case Opcodes.ICONST_0:
+
+					if (fin.desc.equals("Z")) {
+						var.setDefaultValue("false");
+					} else {
+						var.setDefaultValue("0");
+					}
+
+					break;
+
+				case Opcodes.LCONST_0:
+					var.setDefaultValue("0");
+					break;
+
+				case Opcodes.FCONST_0:
+				case Opcodes.DCONST_0:
+					var.setDefaultValue("0.0");
+					break;
+
+				case Opcodes.ICONST_1:
+
+					if (fin.desc.equals("Z")) {
+						var.setDefaultValue("true");
+					} else {
+						var.setDefaultValue("1");
+					}
+
+					break;
+				case Opcodes.LCONST_1:
+					var.setDefaultValue("1");
+					break;
+
+				case Opcodes.FCONST_1:
+				case Opcodes.DCONST_1:
+					var.setDefaultValue("1.0");
+					break;
+
+				case Opcodes.ICONST_2:
+				case Opcodes.FCONST_2:
+					var.setDefaultValue("2");
+					break;
+
+				case Opcodes.ICONST_3:
+					var.setDefaultValue("3");
+					break;
+
+				case Opcodes.ICONST_4:
+					var.setDefaultValue("4");
+					break;
+
+				case Opcodes.ICONST_5:
+					var.setDefaultValue("5");
+					break;
+
+				case Opcodes.BIPUSH:
+				case Opcodes.SIPUSH:
+					var.setDefaultValue("" + ((IntInsnNode) source).operand);
+					break;
+
+				case Opcodes.LDC:
+					var.setDefaultValue(((LdcInsnNode) source).cst.toString());
+					break;
+
+				default:
+					break;
+				}
+
+				break;
+			}
+		}
 	}
 }
