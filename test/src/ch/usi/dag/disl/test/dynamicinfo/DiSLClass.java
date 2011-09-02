d4ae2c7de276ff932f00e623dc991f7ad314a4ec
Yudi Zheng <yudi.zheng@oracle.com>
2011-09-02 16:05:16 +0000

    Fix some typo.

--- test/src/ch/usi/dag/disl/test/dynamicinfo/DiSLClass.java
+++ test/src/ch/usi/dag/disl/test/dynamicinfo/DiSLClass.java
@@ -2,7 +2,6 @@ package ch.usi.dag.disl.test.dynamicinfo;
 
 import ch.usi.dag.disl.dislclass.annotation.AfterReturning;
 import ch.usi.dag.disl.dislclass.annotation.Before;
-import ch.usi.dag.disl.dislclass.snippet.marker.BodyMarker;
 import ch.usi.dag.disl.dislclass.snippet.marker.BytecodeMarker;
 import ch.usi.dag.disl.dynamicinfo.DynamicContext;
 
@@ -15,19 +14,19 @@ public class DiSLClass {
 		System.out.println(i + " - " + j + " = " + (i - j));
 	}
 	
-	@AfterReturning(marker = BodyMarker.class, param = "ireturn", scope = "TargetClass.test1")
+	@AfterReturning(marker = BytecodeMarker.class, param = "ireturn", scope = "TargetClass.test1")
 	public static void postcondition(DynamicContext di) {
 		int ret = di.getLocalVariableValue(1, int.class);
 		System.out.println("before return, local a is " + ret);
 	}
 
-	@AfterReturning(marker = BodyMarker.class, param = "ireturn", scope = "TargetClass.test2")
+	@AfterReturning(marker = BytecodeMarker.class, param = "ireturn", scope = "TargetClass.test2")
 	public static void postcondition2(DynamicContext di) {
 		int ret = di.getStackValue(0, int.class);
 		System.out.println("Return with " + ret);
 	}
 	
-	@AfterReturning(marker = BodyMarker.class, param = "ireturn", scope = "TargetClass.test3")
+	@AfterReturning(marker = BytecodeMarker.class, param = "ireturn", scope = "TargetClass.test3")
 	public static void postcondition3(DynamicContext di) {
 		double d = di.getLocalVariableValue(1, double.class);
 		System.out.println("before return, local d is " + d);
