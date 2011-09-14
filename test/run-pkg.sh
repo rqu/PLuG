5c98bf9139735d87d44362bfd12e64c5ba847117
Lukáš Marek <marek@d3s.mff.cuni.cz>
2011-09-14 13:44:23 +0000

    Updated properties in scripts

--- test/run-pkg.sh
+++ test/run-pkg.sh
@@ -10,4 +10,4 @@ fi
 
 TARGET_CLASS="ch.usi.dag.disl.test.$1.TargetClass"
 
-java -javaagent:../lib/jborat-agent.jar -Dch.usi.dag.jborat.exclusionList="conf/exclusion.lst" -Dch.usi.dag.jborat.liblist="conf/lib.lst" -Dch.usi.dag.jborat.instrumentation="ch.usi.dag.disl.DiSL" -Dch.usi.dag.jborat.codemergerList="conf/codemerger.lst" -Dch.usi.dag.jborat.uninstrumented="uninstrumented" -Dch.usi.dag.jborat.instrumented="instrumented" -Xbootclasspath/p:lib/Thread_test.jar:lib/test-runtime.jar:../lib/jborat-runtime.jar -Ddisl.dynbypass=yes -Ddisl.analysis.umidfile="dico.log" -cp "./bin/"  ${TARGET_CLASS}
+java -javaagent:../lib/jborat-agent.jar -Dch.usi.dag.jborat.exclusionList="conf/exclusion.lst" -Dch.usi.dag.jborat.liblist="conf/lib.lst" -Dch.usi.dag.jborat.instrumentation="ch.usi.dag.disl.DiSL" -Dch.usi.dag.jborat.codemergerList="conf/codemerger.lst" -Dch.usi.dag.jborat.uninstrumented="uninstrumented" -Dch.usi.dag.jborat.instrumented="instrumented" -Xbootclasspath/p:lib/Thread_test.jar:lib/test-runtime.jar:../lib/jborat-runtime.jar -Ddisl.dynbypass -Ddisl.debug -cp "./bin/"  ${TARGET_CLASS}
