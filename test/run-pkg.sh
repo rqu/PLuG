ef6206a7c0f2d11503d90401dcad1ba593d00943
Alex Villazón <alex.villazon@gmail.com>
2011-09-15 20:09:25 +0000

    added conf/udilib.lst and jborat-interface.jar on path

--- test/run-pkg.sh
+++ test/run-pkg.sh
@@ -10,4 +10,15 @@ fi
 
 TARGET_CLASS="ch.usi.dag.disl.test.$1.TargetClass"
 
-java -javaagent:../lib/jborat-agent.jar -Dch.usi.dag.jborat.exclusionList="conf/exclusion.lst" -Dch.usi.dag.jborat.liblist="conf/lib.lst" -Dch.usi.dag.jborat.instrumentation="ch.usi.dag.disl.DiSL" -Dch.usi.dag.jborat.codemergerList="conf/codemerger.lst" -Dch.usi.dag.jborat.uninstrumented="uninstrumented" -Dch.usi.dag.jborat.instrumented="instrumented" -Xbootclasspath/p:lib/Thread_test.jar:lib/test-runtime.jar:../lib/jborat-runtime.jar -Ddisl.dynbypass -Ddisl.debug -cp "./bin/"  ${TARGET_CLASS}
+java -javaagent:../lib/jborat-agent.jar \
+    -Dch.usi.dag.jborat.exclusionList="conf/exclusion.lst" \
+    -Dch.usi.dag.jborat.liblist="conf/lib.lst" \
+    -Dch.usi.dag.jborat.udiliblist="conf/udilib.lst" \
+    -Dch.usi.dag.jborat.instrumentation="ch.usi.dag.disl.DiSL" \
+    -Dch.usi.dag.jborat.codemergerList="conf/codemerger.lst" \
+    -Dch.usi.dag.jborat.uninstrumented="uninstrumented" \
+    -Dch.usi.dag.jborat.instrumented="instrumented" \
+    -Xbootclasspath/p:lib/Thread_test.jar:lib/test-runtime.jar:../lib/jborat-runtime.jar:../lib/jborat-interface.jar \
+    -Ddisl.dynbypass=true \
+    -Ddisl.analysis.umidfile="dico.log" \
+    -cp "./bin/"  ${TARGET_CLASS}
