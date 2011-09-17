36d749ac1be3bd2d628515184282bd12eab82fbb
Alex Villazón <alex.villazon@gmail.com>
2011-09-17 21:07:16 +0000

    Updates threadlocal sample

--- test/src/ch/usi/dag/disl/test/threadlocal/DiSLClass.java
+++ test/src/ch/usi/dag/disl/test/threadlocal/DiSLClass.java
@@ -1,5 +1,6 @@
 package ch.usi.dag.disl.test.threadlocal;
 
+import ch.usi.dag.disl.dislclass.annotation.Before;
 import ch.usi.dag.disl.dislclass.annotation.After;
 import ch.usi.dag.disl.dislclass.annotation.ThreadLocal;
 import ch.usi.dag.disl.dislclass.snippet.marker.BodyMarker;
@@ -9,8 +10,15 @@ public class DiSLClass {
 	@ThreadLocal
 	static String tlv = "ahoj";
 	
-	@After(marker = BodyMarker.class, scope = "TargetClass.foo", order=0)
+	
+	@Before(marker = BodyMarker.class, scope = "*.foo*", order=0)
+	public static void precondition() {
+		System.out.println("pre \t" + Thread.currentThread().toString() + " \t tlv " +  tlv);
+		tlv = "hello";
+	}
+	
+	@After(marker = BodyMarker.class, scope = "*.foo*", order=0)
 	public static void postcondition() {
-		System.out.println(tlv);
+		System.out.println("post \t" + Thread.currentThread().toString() + " \t tlv "  + tlv);
 	}
 }
--- test/src/ch/usi/dag/disl/test/threadlocal/TargetClass.java
+++ test/src/ch/usi/dag/disl/test/threadlocal/TargetClass.java
@@ -3,10 +3,18 @@ package ch.usi.dag.disl.test.threadlocal;
 public class TargetClass {
 	
 	public void foo() {
-
+		
 	}
 	
 	public static void main(String[] args) {
+		 
 		new TargetClass().foo();
+		
+		Thread t = new Thread() {
+			public void run() {
+				new TargetClass().foo();
+			}
+		};
+		t.start();
 	}
 }
