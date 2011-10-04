21ed8775c895f1262d85b9cac15fec34bbb8f0f3
Lukáš Marek <marek@d3s.mff.cuni.cz>
2011-10-04 16:53:03 +0000

    reverted unwanted changes

--- test/src/ch/usi/dag/disl/test/after2/DiSLClass.java
+++ test/src/ch/usi/dag/disl/test/after2/DiSLClass.java
@@ -1,10 +1,8 @@
 package ch.usi.dag.disl.test.after2;
 
-import java.util.LinkedList;
 import java.util.Stack;
 
-import ch.usi.dag.disl.dislclass.annotation.AfterReturning;
-import ch.usi.dag.disl.dislclass.annotation.AfterThrowing;
+import ch.usi.dag.disl.dislclass.annotation.After;
 import ch.usi.dag.disl.dislclass.annotation.Before;
 import ch.usi.dag.disl.dislclass.annotation.ThreadLocal;
 import ch.usi.dag.disl.dislclass.snippet.marker.BodyMarker;
@@ -12,45 +10,20 @@ import ch.usi.dag.disl.staticinfo.analysis.uid.UniqueMethodId;
 import ch.usi.dag.disl.test.after2.runtime.Analysis;
 
 public class DiSLClass {
-    
-	@ThreadLocal
+    @ThreadLocal
     static Stack<Integer> stackTL;
-	
-	@ThreadLocal
-    static LinkedList<String> log;
 
     @Before(marker = BodyMarker.class, order = 0, scope = "*.*", guard = NotInitNorClinit.class)
     public static void onMethodEntryObjectArgs(UniqueMethodId id) {
-        
-    	Stack<Integer> thisStack = null;
-        
+        Stack<Integer> thisStack;
         if((thisStack = stackTL) == null) {
             thisStack = (stackTL = new Stack<Integer>());
         }
-        
-        int idN = id.get();
-        
-        thisStack.push(idN);
-
-        LinkedList<String> thisLog = null;
-        
-        if((thisLog = log) == null) {
-            thisLog = (log = new LinkedList<String>());
-        }
-        
-        Analysis.log(thisLog, "Up " + idN);
+        thisStack.push(id.get());
     }
 
-    @AfterReturning(marker = BodyMarker.class, order = 0, scope = "*.*", guard = NotInitNorClinit.class)
-    public static void onMethodRet(UniqueMethodId id) {
-    	
-        Analysis.onExit(stackTL, id.get(), log, "DownOk ");
-    }
-    
-    @AfterThrowing(marker = BodyMarker.class, order = 0, scope = "*.*", guard = NotInitNorClinit.class)
-    public static void onMethodExc(UniqueMethodId id) {
-    	
-    	Analysis.onExit(stackTL, id.get(), log, "DownEx ");
-        
+    @After(marker = BodyMarker.class, order = 0, scope = "*.*", guard = NotInitNorClinit.class)
+    public static void onMethodExit(UniqueMethodId id) {
+        Analysis.onExit(stackTL, id.get());
     }
 }
