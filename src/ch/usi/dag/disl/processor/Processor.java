67d9ae9ef4327de2f5cce05ec6400d83dc7d830f
Lukáš Marek <marek@d3s.mff.cuni.cz>
2011-11-15 18:31:40 +0000

    Refined processor interface

--- src/ch/usi/dag/disl/processor/Processor.java
+++ src/ch/usi/dag/disl/processor/Processor.java
@@ -6,14 +6,31 @@ public abstract class Processor {
 	 * Applies mentioned processor for method or call-site arguments
 	 * 
 	 * @param processorClass processor class to apply
-	 * @param applyType mode in which should be processor applied
+	 * @param mode in which should be processor applied
 	 */
-	public static void apply(Class<?> processorClass, ProcessorMode applyType) {
+	public static void apply(Class<?> processorClass, ProcessorMode mode) {
 		
 	}
 
-	// TODO ! receiver - should it have apply type?, add description
-	public static Object getReceiver(ProcessorMode applyType) {
+	// TODO ! add support
+	/**
+	 * Returns the object on which is the processed method (arguments of that
+	 * method) called.
+	 * 
+	 * @param mode for which should be the object retrieved
+	 */
+	public static Object getReceiver(ProcessorMode mode) {
+		return null;
+	}
+	
+	// TODO ! add support
+	/**
+	 * Returns the object array composed from the method arguments. Note that
+	 * primitive types will be boxed.
+	 * 
+	 * @param mode for which should be the argument array retrieved
+	 */
+	public static Object[] getArgs(ProcessorMode mode) {
 		return null;
 	}
 }
