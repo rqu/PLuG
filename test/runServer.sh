50bbc9d6ea8c8cf8bc16109af69d891b5bad711b
Alex Villazón <alex.villazon@gmail.com>
2011-11-17 18:21:06 +0000

    updated to jborat 0.10 with shm No need to manually copy the library (.so or .jnilib)...

--- test/runServer.sh
+++ test/runServer.sh
@@ -1,11 +1,34 @@
 #!/bin/sh
 
+OS=`uname`
+ARCH_RAW=`uname -m`
+ARCH="unspec"
+
+if [ "${ARCH_RAW}" == "i686" -o "${ARCH_RAW}" == "x86_32" ]; then
+  ARCH="x32"
+elif [ "${ARCH_RAW}" == "x86_64" ]; then
+  ARCH="x64"
+else
+  echo "Unknow architecture.."
+  exit -1
+fi 
+
+if [ "${OS}" == "Darwin" ]; then
+  JBORAT_AGENT_PATH="macosx/${ARCH}"
+elif  [ "${OS}" == "Linux" ]; then
+  JBORAT_AGENT_PATH="linux/${ARCH}"
+else
+  echo "Unknow platform..."
+  exit -1
+fi
+
 CLASSPATH=../lib/jborat-agent.jar:../lib/jborat-runtime.jar:../lib/jborat-interface.jar:lib/remote-server.jar
 
 java -Dch.usi.dag.jborat.instrumented="instrumented" \
     -Djborat.debug \
     -Ddisl.dynbypass=true \
     -Ddisl.debug \
+    -Djava.library.path=./lib/${JBORAT_AGENT_PATH} \
     -Dch.usi.dag.jborat.instrumentation="ch.usi.dag.disl.DiSL" \
     -Dch.usi.dag.jborat.codemergerList="conf/codemerger.lst" \
     -Dch.usi.dag.jborat.liblist="conf/lib.lst" \
