9cc8b0eee3f1dc24eb2617764d38b65943943856
Yudi Zheng <yudi.zheng@oracle.com>
2011-11-20 09:02:29 +0000

    Weaver does not support boxing and un-boxing now.

--- test/src/ch/usi/dag/disl/test/processor/ProcessorTest2.java
+++ test/src/ch/usi/dag/disl/test/processor/ProcessorTest2.java
@@ -10,12 +10,14 @@ public class ProcessorTest2 {
 
 	@SyntheticLocal
 	public static String flag;
-	
+
 	public static void objPM(Object c, ArgumentContext ac, DynamicContext dc) {
+		int index = dc.stackValue(1, int.class);
+
 		System.out.println("Testing dynamic context in processor");
-		System.out.println("Accessed array index is " + dc.stackValue(1, int.class));
+		System.out.println("Accessed array index is " + index);
 		System.out.println("--------------------");
-		
+
 		DiSLClass.flag = "OMG this is for the End";
 	}
 }
