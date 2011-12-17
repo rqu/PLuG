30133defe45237f636352d311a75315aee411c3c
Yudi Zheng <yudi.zheng@oracle.com>
2011-12-17 02:50:43 +0000

    Test case for Invocation Interpreter.

---
+++ test/src/ch/usi/dag/disl/test/pe2/TargetClass.java
@@ -0,0 +1,18 @@
+package ch.usi.dag.disl.test.pe2;
+
+public class TargetClass {
+
+	private void private_method() {
+		System.out.println("Inside TargetClass.private_method.");
+	}
+
+	public void public_method() {
+		System.out.println("Inside TargetClass.public_method.");
+	}
+
+	public static void main(String[] args) {
+		TargetClass t = new TargetClass();
+		t.private_method();
+		t.public_method();
+	}
+}
