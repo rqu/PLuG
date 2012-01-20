289923c5415464499552a636b02af1eb0c38e40d
Lukáš Marek <marek@d3s.mff.cuni.cz>
2012-01-20 11:54:11 +0000

    dropped test directory

--- test/run-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-TESTS=`ls src/ch/usi/dag/disl/test/`
-
-for TEST in ${TESTS}
-do
-	echo "*** Starting test ${TEST} ***"
-	./run.sh ${TEST} pkg 2>&1 | tee -a run-all.log
-done
--- test/src/ch/usi/dag/disl/test/symbolic/symbol/Symbol.java
+++
@@ -1,9 +0,0 @@
-package ch.usi.dag.disl.test.symbolic.symbol;
-
-import stp.Expr;
-import stp.VC;
-
-public interface Symbol {
-	
-	public Expr translate(VC vc);
-}
