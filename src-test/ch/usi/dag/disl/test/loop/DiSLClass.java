bba93b6533f34002e5a4b8d97938a39b91a8740a
Yudi Zheng <yudi.zheng@oracle.com>
2012-04-20 12:26:24 +0000

    Revised test case for loop.

--- src-test/ch/usi/dag/disl/test/loop/DiSLClass.java
+++ src-test/ch/usi/dag/disl/test/loop/DiSLClass.java
@@ -2,13 +2,13 @@ package ch.usi.dag.disl.test.loop;
 
 import ch.usi.dag.disl.annotation.Before;
 import ch.usi.dag.disl.marker.BasicBlockMarker;
-import ch.usi.dag.disl.staticcontext.BasicBlockStaticContext;
+import ch.usi.dag.disl.staticcontext.LoopStaticContext;
 
 public class DiSLClass {
 
 	@Before(marker = BasicBlockMarker.class, scope = "TargetClass.print()", order = 2)
-	public static void precondition(BasicBlockStaticContext bba) {
-		System.out.println("Enter basic block ! index: " + bba.getBBindex()
-				+ " loopstart? " + (bba.isFirstOfLoop() ? "true" : "false"));
+	public static void precondition(LoopStaticContext lsc) {
+		System.out.println("Enter basic block ! index: " + lsc.getBBindex()
+				+ " loopstart? " + (lsc.isFirstOfLoop() ? "true" : "false"));
 	}
 }
