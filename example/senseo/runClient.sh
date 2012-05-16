cae98d0c73be19df31881a8312dc1c06bc2e303a
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2012-05-16 18:46:13 +0000

    Senseo example without basic-block analysis.

--- example/senseo/runClient.sh
+++ example/senseo/runClient.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
 
 OS=`uname`
-ARCH_RAW=`uname -m`
-ARCH="unspec"
-
-if [ "${ARCH_RAW}" = "i686" -o "${ARCH_RAW}" = "x86_32" ]; then
-  ARCH="x32"
-elif [ "${ARCH_RAW}" = "x86_64" ]; then
-  ARCH="x64"
-else
-  echo "Unknow architecture...."
-  exit -1
-fi
 
 if [ "${OS}" = "Darwin" ]; then
-  JBORAT_AGENT="macosx/${ARCH}/"libjboratagent.jnilib
-elif  [ "${OS}" = "Linux" ]; then
-  JBORAT_AGENT="linux/${ARCH}/"libjboratagent.so
+  C_AGENT="../../src-agent-c/libdislagent.jnilib"
 else
-  echo "Unknow platform..."
-  exit -1
+  C_AGENT="../../src-agent-c/libdislagent.so"
 fi
 
-# shared memory is the default communication
-# to enable socket, add ",ipc.socket" to the options
-JBORAT_AGENT_OPTS="1234,localhost"
-
-java -XX:MaxPermSize=128m \
-    -Xmx2G \
-    -javaagent:../../test/lib/agent.jar \
-    -Xbootclasspath/p:../../test/lib/remote-runtime.jar:../../lib/jborat-runtime.jar:./build/senseo-runtime.jar \
-    -agentpath:../../test/lib/${JBORAT_AGENT}=${JBORAT_AGENT_OPTS} \
-    -cp ./bin \
+java \
+     -Xmx4G \
+     -XX:MaxPermSize=128m \
+     -agentpath:${C_AGENT} \
+     -javaagent:../../build/dislagent-unspec.jar \
+     -Xbootclasspath/a:../../build/dislagent-unspec.jar:../../build/dislinstr.jar \
+     -cp bin/ \
      $*
