116746b0ede68cbffa3583bb377ef31024d6a368
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2012-08-15 13:28:00 +0000

    Removed the example directory.

--- example/jcarder/src/ch/usi/dag/disl/example/jcarder/MyMethodStaticContext.java
+++
@@ -1,9 +0,0 @@
-package ch.usi.dag.disl.example.jcarder;
-
-import ch.usi.dag.disl.staticcontext.MethodStaticContext;
-
-public class MyMethodStaticContext extends MethodStaticContext {
-
-	
-
-}
--- example/senseo/src/ch/usi/dag/disl/example/senseo/runtime/arguments/Null.java
+++
@@ -1,7 +0,0 @@
-package ch.usi.dag.disl.example.senseo.runtime.arguments;
-
-public class Null implements PrimitiveWrapper {
-    public java.lang.String toString() {
-        return "null";
-    }
-}
