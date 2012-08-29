af681680bb1c7a72d39d74f9437af6b42d1fbed0
Lukáš Marek <marek@d3s.mff.cuni.cz>
2012-08-29 17:55:31 +0000

    Reverted changes introducing reverse class loading -r514:488 on /branches/remoteval/src-agent-c; buggy unused code

--- src-agent-c/dislagent.c
+++ src-agent-c/dislagent.c
@@ -13,7 +13,6 @@
 
 #include "jvmtihelper.h"
 #include "comm.h"
-#include "classparser.h"
 
 #include "dislagent.h"
 
@@ -371,65 +370,6 @@ static message instrument_class(const char * classname,
 	return result;
 }
 
-// load super class
-static void load_super_class(JNIEnv* jni_env, jobject loader, const char* name,
-		const unsigned char* class_data) {
-
-	// allocate space for constant pool table
-	unsigned short cpcount = read_short(class_data, CPCOUNT_OFFSET);
-	unsigned int * cp = (unsigned int *) malloc(sizeof(int) * cpcount);
-
-	// construct constant pool and calculate offset of super class identifier
-	unsigned int class_off = get_constant_pool(class_data, cp, cpcount) + 4;
-	unsigned int class_idx = read_short(class_data, class_off);
-
-	if (class_idx == 0) {
-		// loading or redefining java.lang.Object, which has no super class
-		// free constant pool table
-		free((void *) cp);
-		cp = NULL;
-		return;
-	}
-
-	// super class name in constant pool, which is a "UTF-8"-type entry
-	unsigned int name_idx = read_short(class_data, cp[class_idx] + 1);
-	unsigned int name_off = cp[name_idx];
-	unsigned int name_len = read_short(class_data, name_off + 1);
-	char * name_src = ((char *) class_data) + name_off + 3;
-
-	// free constant pool table
-	free((void *) cp);
-	cp = NULL;
-
-	// copy super class name from constant pool
-	char * buf = (char *) malloc(name_len + 1);
-	strncpy(buf, name_src, name_len);
-	buf[name_len] = '\0';
-
-	// load super class
-	if (loader == NULL) {
-	    (*jni_env)->FindClass(jni_env, buf);
-	}
-	else {
-	    char * binaryName = (char *) malloc(name_len + 1);
-	    strncpy(binaryName, buf, name_len + 1);
-	    unsigned int i;
-	    for (i = 0; i < name_len; i++) {
-	        if (binaryName[i] == '/') {
-	            binaryName[i] = '.';
-	        }
-	    }
-	    jclass cls = (*jni_env)->GetObjectClass(jni_env, loader);
-	    jmethodID mid = (*jni_env)->GetMethodID(jni_env, cls, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
-	    jstring jclassName = (*jni_env)->NewStringUTF(jni_env, binaryName);
-	    (*jni_env)->CallObjectMethod(jni_env, loader, mid, jclassName);
-	}
-
-	// free super class name
-	free((void *) buf);
-	buf = NULL;
-}
-
 // ******************* CLASS LOAD callback *******************
 
 void JNICALL jvmti_callback_class_file_load_hook( jvmtiEnv *jvmti_env,
@@ -446,11 +386,6 @@ void JNICALL jvmti_callback_class_file_load_hook( jvmtiEnv *jvmti_env,
 		printf("Instrumenting unknown class\n");
 	}
 #endif
-
-	//TODO: force loading of super classes
-//	if (jni_env != NULL) {
-//		load_super_class(jni_env, loader, name, class_data);
-//	}
 
 	// ask the server to instrument
 	message instrclass = instrument_class(name, class_data, class_data_len);
