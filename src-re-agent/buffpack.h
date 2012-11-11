a251e2617f46957289db99f4a63df7922a6da677
Lubomir Bulej <bulej@d3s.mff.cuni.cz>
2012-11-11 02:14:42 +0000

    DiSL-RE agent: cosmetic cleanup in buffpack.h

--- src-re-agent/buffpack.h
+++ src-re-agent/buffpack.h
@@ -20,35 +20,29 @@
 #include "buffer.h"
 
 void pack_boolean(buffer * buff, jboolean to_send) {
-
 	buffer_fill(buff, &to_send, sizeof(jboolean));
 }
 
 void pack_byte(buffer * buff, jbyte to_send) {
-
 	buffer_fill(buff, &to_send, sizeof(jbyte));
 }
 
 void pack_char(buffer * buff, jchar to_send) {
-
 	jchar nts = htons(to_send);
 	buffer_fill(buff, &nts, sizeof(jchar));
 }
 
 void pack_short(buffer * buff, jshort to_send) {
-
 	jshort nts = htons(to_send);
 	buffer_fill(buff, &nts, sizeof(jshort));
 }
 
 void pack_int(buffer * buff, jint to_send) {
-
 	jint nts = htonl(to_send);
 	buffer_fill(buff, &nts, sizeof(jint));
 }
 
 void pack_long(buffer * buff, jlong to_send) {
-
 	jlong nts = htobe64(to_send);
 	buffer_fill(buff, &nts, sizeof(jlong));
 }
@@ -64,7 +58,6 @@ void pack_string_utf8(buffer * buff, const void * string_utf8,
 }
 
 void pack_bytes(buffer * buff, const void * data, jint size) {
-
 	buffer_fill(buff, data, size);
 }
 
