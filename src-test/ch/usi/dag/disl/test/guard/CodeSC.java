ed81a68ff14d92a0fdbfd431d4944d1a695b7684
Lubomir Bulej <bulej@d3s.mff.cuni.cz>
2012-11-11 23:49:09 +0000

    DiSL: CodeSC in guard test: don't copy instructions just to determine code size

--- src-test/ch/usi/dag/disl/test/guard/CodeSC.java
+++ src-test/ch/usi/dag/disl/test/guard/CodeSC.java
@@ -2,11 +2,11 @@ package ch.usi.dag.disl.test.guard;
 
 import ch.usi.dag.disl.staticcontext.AbstractStaticContext;
 
+
 public class CodeSC extends AbstractStaticContext {
 
-	public int codeLength() {
-		
-		return staticContextData.getMethodNode().instructions.toArray()
-				.length;
-	}
+    public int codeLength () {
+        return staticContextData.getMethodNode ().instructions.size ();
+    }
+    
 }
