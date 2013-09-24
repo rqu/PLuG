08034aa0132cd1009fdd7b0a0115501542122731
František Haas <frantisek.haas@gmail.com>
2013-09-24 09:29:56 +0000

    added eclipse formatter, importorder and cleanup profile

--- src-test/ch/usi/dag/disl/test2/suite/after/app/TargetClass.java
+++ src-test/ch/usi/dag/disl/test2/suite/after/app/TargetClass.java
@@ -1,43 +1,40 @@
 package ch.usi.dag.disl.test2.suite.after.app;
 
 public class TargetClass {
-	
-	public void print(boolean flag) {
-		try {
-			System.out.println("app: TargetClass.print(..) - try:begin");
 
-			if (flag) {
-				String float_one = "1.0";
-				int int_one = Integer.valueOf(float_one);
-				System.out.println("app: UNREACHABLE " + int_one);
-			}
+    public void print(boolean flag) {
+        try {
+            System.out.println("app: TargetClass.print(..) - try:begin");
 
-			System.out.println("app: TargetClass.print(..) - try:end");
-		} finally {
-			System.out.println("app: TargetClass.print(..) - finally");
-		}
-	}
-	
-	// FIXME
-	// this main breaks the test on class not found exception of this class
-	/*
-	public static void main(String[] args) {
-		try {
-			TargetClass t = new TargetClass();
-			System.out.println("app: TargetClass.main(..) - .print(false)");
-			t.print(false);
-			System.out.println("app: TargetClass.main(..) - .print(true)");
-			t.print(true);
-		} catch (Throwable e) {
-			System.out.println("app: TargetClass.main(..) - catch");
-		}
-	}*/
-	
-	public static void main(String[] args) {
-		TargetClass t = new TargetClass();
-		System.out.println("app: TargetClass.main(..) - .print(false)");
-		t.print(false);
-		System.out.println("app: TargetClass.main(..) - .print(true)");
-		t.print(true);
-	}
+            if (flag) {
+                String float_one = "1.0";
+                Integer int_one = Integer.valueOf(float_one);
+                System.out.println("app: UNREACHABLE " + int_one);
+            }
+
+            System.out.println("app: TargetClass.print(..) - try:end");
+        } finally {
+            System.out.println("app: TargetClass.print(..) - finally");
+        }
+    }
+
+    // FIXME
+    // this main breaks the test on class not found exception of this class
+    /*
+     * public static void main(String[] args) { try { TargetClass t = new
+     * TargetClass();
+     * System.out.println("app: TargetClass.main(..) - .print(false)");
+     * t.print(false);
+     * System.out.println("app: TargetClass.main(..) - .print(true)");
+     * t.print(true); } catch (Throwable e) {
+     * System.out.println("app: TargetClass.main(..) - catch"); } }
+     */
+
+    public static void main(String[] args) {
+        TargetClass t = new TargetClass();
+        System.out.println("app: TargetClass.main(..) - .print(false)");
+        t.print(false);
+        System.out.println("app: TargetClass.main(..) - .print(true)");
+        t.print(true);
+    }
 }
--- src-test/ch/usi/dag/disl/test2/suite/after/junit/AfterTest.java
+++ src-test/ch/usi/dag/disl/test2/suite/after/junit/AfterTest.java
@@ -7,34 +7,35 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import ch.usi.dag.disl.test2.utils.*;
+import ch.usi.dag.disl.test2.utils.ClientServerRunner;
 
 @RunWith(JUnit4.class)
 public class AfterTest {
 
-	ClientServerRunner r = new ClientServerRunner(this.getClass());
-	
-	@Test
-	public void test() 
-			throws Exception {
-		// FIXME
-		fail("FIXME");
-		// see app TargetClass
+    ClientServerRunner r = new ClientServerRunner(this.getClass());
 
-		r.start();			
-		r.waitFor();
-		r.assertIsFinished();
-		if(Boolean.parseBoolean(System.getProperty("disl.test.verbose"))) { r.destroyIfRunningAndFlushOutputs(); }
-		//r.assertIsSuccessfull();
-		r.assertClientOut("client.out.resource");
-		//r.assertClientErrNull();
-		r.assertServerOutNull();
-		r.assertServerErrNull();
-	}	
-	
-	@After
-	public void cleanup() {
-		r.destroy();
-	}
+    @Test
+    public void test(int i)
+            throws Exception {
+        // FIXME
+        fail("FIXME");
+        // see app TargetClass
+
+        r.start();
+        r.waitFor();
+        r.assertIsFinished();
+        if (Boolean.parseBoolean(System.getProperty("disl.test.verbose"))) {
+            r.destroyIfRunningAndFlushOutputs();
+        }
+        // r.assertIsSuccessfull();
+        r.assertClientOut("client.out.resource");
+        // r.assertClientErrNull();
+        r.assertServerOutNull();
+        r.assertServerErrNull();
+    }
+
+    @After
+    public void cleanup() {
+        r.destroy();
+    }
 }
-
