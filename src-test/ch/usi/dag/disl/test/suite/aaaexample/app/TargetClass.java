c6f513f4f9ef031d021ec52fa5b75d1881f683e1
František Haas <frantisek.haas@gmail.com>
2013-10-09 19:36:56 +0000

    moved test2 to test

---
+++ src-test/ch/usi/dag/disl/test/suite/aaaexample/app/TargetClass.java
@@ -0,0 +1,8 @@
+package ch.usi.dag.disl.test.suite.aaaexample.app;
+
+public class TargetClass {
+
+    public static void main(String[] args) {
+        System.out.println("app: TargetClass.main(..)");
+    }
+}
---
+++ src-test/ch/usi/dag/disl/test/suite/loop/junit/LoopTest.java
@@ -0,0 +1,41 @@
+package ch.usi.dag.disl.test.suite.loop.junit;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import ch.usi.dag.disl.test.utils.ClientServerRunner;
+
+@RunWith(JUnit4.class)
+public class LoopTest {
+
+	ClientServerRunner r = new ClientServerRunner(this.getClass());
+
+	@Test
+	public void test()
+			throws Exception {
+		fail("FIXME - nondeterministic");
+		// FIXME
+		// see client.out.1.resource and client.out.2.resource
+		// test might nondeterministically result in both of them
+
+		r.start();
+		r.waitFor();
+		r.assertIsFinished();
+		if(Boolean.parseBoolean(System.getProperty("disl.test.verbose"))) { r.destroyIfRunningAndFlushOutputs(); }
+		r.assertIsSuccessfull();
+		r.assertClientOut("client.out.resource");
+		r.assertClientErrNull();
+		r.assertServerOutNull();
+		r.assertServerErrNull();
+	}
+
+	@After
+	public void cleanup() {
+		r.destroy();
+	}
+}
+
