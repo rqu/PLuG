5ad440fb8c0e5427c77f4e172aea661a1e7710d3
František Haas <frantisek.haas@gmail.com>
2013-10-10 12:14:19 +0000

    removed files committed by mistake

--- src-test/ch/usi/dag/disl/test/suite/aaaexample/app/TargetClass.java
+++
@@ -1,8 +0,0 @@
-package ch.usi.dag.disl.test.suite.aaaexample.app;
-
-public class TargetClass {
-
-    public static void main(String[] args) {
-        System.out.println("app: TargetClass.main(..)");
-    }
-}
