49c13354d1a08ee5c0839449edb0e8c88975a892
Lubomir Bulej <bulej@d3s.mff.cuni.cz>
2014-07-22 15:12:44 +0000

    TLVInserter, UniqueMethodId: cleaned up white space before merging changes.

--- src-disl/ch/usi/dag/disl/staticcontext/uid/UniqueMethodId.java
+++ src-disl/ch/usi/dag/disl/staticcontext/uid/UniqueMethodId.java
@@ -7,37 +7,37 @@ import ch.usi.dag.disl.util.Constants;
 
 public class UniqueMethodId extends AbstractUniqueId {
 
-	private static AbstractUniqueId instance = null;
-	
-	// constructor for static context
-	public UniqueMethodId() {
-		
-	}
-	
-	// constructor for singleton
-	private UniqueMethodId(AbstractIdCalculator idCalc,
-			String outputFileName) {
-		super(idCalc, outputFileName);
-	}
-	
-	protected String idFor() {
-		
-		ClassNode classNode = staticContextData.getClassNode();
-		MethodNode methodNode = staticContextData.getMethodNode();
-		
-		return classNode.name
-				+ Constants.CLASS_DELIM
-				+ methodNode.name
-				+ "("
-				+ methodNode.desc
-				+ ")";
-	}
-	
-	protected synchronized AbstractUniqueId getSingleton() {
-		
-		if (instance == null) {
-			instance = new UniqueMethodId(new RandomId(), "methodid.txt");
-		}
-		return instance;
-	}
+    private static AbstractUniqueId instance = null;
+
+    // constructor for static context
+    public UniqueMethodId() {
+
+    }
+
+    // constructor for singleton
+    private UniqueMethodId(AbstractIdCalculator idCalc,
+            String outputFileName) {
+        super(idCalc, outputFileName);
+    }
+
+    protected String idFor() {
+
+        ClassNode classNode = staticContextData.getClassNode();
+        MethodNode methodNode = staticContextData.getMethodNode();
+
+        return classNode.name
+                + Constants.CLASS_DELIM
+                + methodNode.name
+                + "("
+                + methodNode.desc
+                + ")";
+    }
+
+    protected synchronized AbstractUniqueId getSingleton() {
+
+        if (instance == null) {
+            instance = new UniqueMethodId(new RandomId(), "methodid.txt");
+        }
+        return instance;
+    }
 }
