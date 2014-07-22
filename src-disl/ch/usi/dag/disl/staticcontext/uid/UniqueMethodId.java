80a4ad740460aa5277557557e1463e70136d4ab9
Lubomir Bulej <bulej@d3s.mff.cuni.cz>
2014-07-22 15:56:48 +0000

    UniqueMethodId: made this class deprecated in favor of RandomMethodUid. SequentialMethodUid: new class to provide sequential method identifiers. RandomMethodUid: companion class to SequentialMethodUid providing random method identifiers. Replaces UniqueMethodId class. AbstractMethodUid: contains common parts for SequentialMethodUid and RandomMethodUid.

--- src-disl/ch/usi/dag/disl/staticcontext/uid/UniqueMethodId.java
+++ src-disl/ch/usi/dag/disl/staticcontext/uid/UniqueMethodId.java
@@ -1,43 +1,15 @@
 package ch.usi.dag.disl.staticcontext.uid;
 
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodNode;
-
-import ch.usi.dag.disl.util.Constants;
-
-public class UniqueMethodId extends AbstractUniqueId {
-
-    private static AbstractUniqueId instance = null;
+/**
+ * This class has been deprecated in favor of the {@link RandomMethodUid} and
+ * will be eliminated in future.
+ */
+@Deprecated
+public class UniqueMethodId extends RandomMethodUid {
 
     // constructor for static context
-    public UniqueMethodId() {
-
+    public UniqueMethodId () {
+        super ();
     }
 
-    // constructor for singleton
-    private UniqueMethodId(AbstractIdCalculator idCalc,
-            String outputFileName) {
-        super(idCalc, outputFileName);
-    }
-
-    protected String idFor() {
-
-        ClassNode classNode = staticContextData.getClassNode();
-        MethodNode methodNode = staticContextData.getMethodNode();
-
-        return classNode.name
-                + Constants.CLASS_DELIM
-                + methodNode.name
-                + "("
-                + methodNode.desc
-                + ")";
-    }
-
-    protected synchronized AbstractUniqueId getSingleton() {
-
-        if (instance == null) {
-            instance = new UniqueMethodId(new RandomId(), "methodid.txt");
-        }
-        return instance;
-    }
 }
