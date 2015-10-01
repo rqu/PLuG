06e51488c2fefb43fedc2aef805be745e954dc5e
Lubomir Bulej <bulej@d3s.mff.cuni.cz>
2015-10-01 20:09:48 +0000

    Updated the thread local test to cover all primitive types and their initialization.

--- src-test/ch/usi/dag/disl/test/suite/threadlocal/app/TargetClass.java
+++ src-test/ch/usi/dag/disl/test/suite/threadlocal/app/TargetClass.java
@@ -1,20 +1,21 @@
 package ch.usi.dag.disl.test.suite.threadlocal.app;
 
 public class TargetClass {
-	
+
 	public void foo() {
-		
+	    // to be instrumented
 	}
-	
-	public static void main(String[] args) {
-		 
+
+	public static void main(final String[] args) {
+        Thread.currentThread().setName ("primary");
 		new TargetClass().foo();
-		
-		Thread t = new Thread() {
-			public void run() {
+
+		new Thread() {
+			@Override
+            public void run() {
+			    setName ("secondary");
 				new TargetClass().foo();
 			}
-		};
-		t.start();
+		}.start ();
 	}
 }
