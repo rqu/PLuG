83a800fa612ab75e95e3a687d5040208a066af3d
Lubomir Bulej <bulej@d3s.mff.cuni.cz>
2016-02-17 13:33:09 +0000

    LoopStaticContext: unify white space before further changes.

--- src-disl/ch/usi/dag/disl/staticcontext/LoopStaticContext.java
+++ src-disl/ch/usi/dag/disl/staticcontext/LoopStaticContext.java
@@ -19,83 +19,83 @@ import ch.usi.dag.disl.util.cfg.CtrlFlowGraph;
  */
 public class LoopStaticContext extends BasicBlockStaticContext {
 
-	private Map<BasicBlock, Set<BasicBlock>> dominatormapping;
+    private Map<BasicBlock, Set<BasicBlock>> dominatormapping;
 
-	@Override
-	protected CtrlFlowGraph produceCustomData() {
+    @Override
+    protected CtrlFlowGraph produceCustomData() {
 
-		MethodNode method = staticContextData.getMethodNode();
-		CtrlFlowGraph cfg = CtrlFlowGraph.build(method);
+        MethodNode method = staticContextData.getMethodNode();
+        CtrlFlowGraph cfg = CtrlFlowGraph.build(method);
 
-		dominatormapping = new HashMap<BasicBlock, Set<BasicBlock>>();
+        dominatormapping = new HashMap<BasicBlock, Set<BasicBlock>>();
 
-		Set<BasicBlock> entries = new HashSet<BasicBlock>();
-		entries.add(cfg.getBB(method.instructions.getFirst()));
+        Set<BasicBlock> entries = new HashSet<BasicBlock>();
+        entries.add(cfg.getBB(method.instructions.getFirst()));
 
-		for (TryCatchBlockNode tcb : method.tryCatchBlocks) {
-			entries.add(cfg.getBB(tcb.handler));
-		}
+        for (TryCatchBlockNode tcb : method.tryCatchBlocks) {
+            entries.add(cfg.getBB(tcb.handler));
+        }
 
-		for (BasicBlock bb : cfg.getNodes()) {
+        for (BasicBlock bb : cfg.getNodes()) {
 
-			Set<BasicBlock> dominators = new HashSet<BasicBlock>();
+            Set<BasicBlock> dominators = new HashSet<BasicBlock>();
 
-			if (entries.contains(bb)) {
-				dominators.add(bb);
-			} else {
-				dominators.addAll(cfg.getNodes());
-			}
+            if (entries.contains(bb)) {
+                dominators.add(bb);
+            } else {
+                dominators.addAll(cfg.getNodes());
+            }
 
-			dominatormapping.put(bb, dominators);
-		}
+            dominatormapping.put(bb, dominators);
+        }
 
-		// whether the dominators of any basic block is changed
-		boolean changed;
+        // whether the dominators of any basic block is changed
+        boolean changed;
 
-		// loop until no more changes
-		do {
-			changed = false;
+        // loop until no more changes
+        do {
+            changed = false;
 
-			for (BasicBlock bb : cfg.getNodes()) {
+            for (BasicBlock bb : cfg.getNodes()) {
 
-				if (entries.contains(bb)) {
-					continue;
-				}
+                if (entries.contains(bb)) {
+                    continue;
+                }
 
-				Set<BasicBlock> dominators = dominatormapping.get(bb);
-				dominators.remove(bb);
+                Set<BasicBlock> dominators = dominatormapping.get(bb);
+                dominators.remove(bb);
 
-				// update the dominators of current basic block,
-				// contains only the dominators of its predecessors
-				for (BasicBlock predecessor : bb.getPredecessors()) {
+                // update the dominators of current basic block,
+                // contains only the dominators of its predecessors
+                for (BasicBlock predecessor : bb.getPredecessors()) {
 
-					if (dominators.retainAll(dominatormapping.get(predecessor))) {
-						changed = true;
-					}
-				}
+                    if (dominators.retainAll(dominatormapping.get(predecessor))) {
+                        changed = true;
+                    }
+                }
 
-				dominators.add(bb);
-			}
-		} while (changed);
+                dominators.add(bb);
+            }
+        } while (changed);
 
-		return cfg;
-	}
+        return cfg;
+    }
 
-	/**
-	 * Returns true if the instrumented instruction is start of a loop.
-	 */
-	public boolean isFirstOfLoop() {
+    /**
+     * Returns true if the instrumented instruction is start of a loop.
+     */
+    public boolean isFirstOfLoop() {
 
-		BasicBlock entry = customData.getBB(staticContextData
-				.getRegionStart());
+        BasicBlock entry = customData.getBB(staticContextData
+                .getRegionStart());
 
-		for (BasicBlock bb : entry.getPredecessors()) {
-			if (dominatormapping.get(bb).contains(entry)) {
-				return true;
-			}
-		}
+        for (BasicBlock bb : entry.getPredecessors()) {
+            if (dominatormapping.get(bb).contains(entry)) {
+                return true;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
 }
